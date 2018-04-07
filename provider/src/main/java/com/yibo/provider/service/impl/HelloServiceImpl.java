bc9eb000205587644378ac50639bebcdabbd90e8
XuYibo <xuyibo@anji-plus.com>
2018-04-07 12:54:42 +0800

    使用zookeeper作为注册中心

--- provider/src/main/java/com/yibo/provider/service/impl/HelloServiceImpl.java
+++ provider/src/main/java/com/yibo/provider/service/impl/HelloServiceImpl.java
@@ -2,6 +2,8 @@ package com.yibo.provider.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.yibo.provider.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service(
         version = "2.1",
@@ -10,8 +12,10 @@ import com.yibo.provider.service.HelloService;
         registry = "${dubbo.registry.id}"
 )
 public class HelloServiceImpl implements HelloService {
+    private static final Logger LOGGER=LoggerFactory.getLogger(HelloServiceImpl.class);
     @Override
     public String hello(String name) {
+        LOGGER.info("接受到参数:"+name);
         return "Hello, " + name;
     }
 }
