81ddffcc28d65b1ce718a05ddceba45c2393657d
Yibo <xuyibo@outlook.com>
2018-09-06 10:47:34 +0800

    组件升级

--- anotherprovider/src/main/java/com/yibo/anotherprovider/service/impl/HiServiceImpl.java
+++ anotherprovider/src/main/java/com/yibo/anotherprovider/service/impl/HiServiceImpl.java
@@ -5,7 +5,7 @@ import com.yibo.provider.service.HiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Service(protocol = "${dubbo.protocol.id}")
+@Service
 public class HiServiceImpl implements HiService {
     private static final Logger LOGGER = LoggerFactory.getLogger(HiServiceImpl.class);
 
