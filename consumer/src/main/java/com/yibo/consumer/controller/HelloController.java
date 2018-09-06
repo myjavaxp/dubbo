b136554bf440194e38b5e695a9668bf57659943f
Yibo <xuyibo@outlook.com>
2018-09-06 11:01:08 +0800

    组件升级

--- consumer/src/main/java/com/yibo/consumer/controller/HelloController.java
+++ consumer/src/main/java/com/yibo/consumer/controller/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
     @Reference
@@ -15,17 +18,23 @@ public class HelloController {
     private HiService hiService;
 
     @GetMapping("/hello/{name}")
-    public String hello(@PathVariable("name") String name) {
-        return helloService.hello(name);
+    public Map<String, String> hello(@PathVariable("name") String name) {
+        Map<String, String> map = new HashMap<>(1);
+        map.put("msg", helloService.hello(name));
+        return map;
     }
 
     @GetMapping("/hi/{name}")
-    public String hi(@PathVariable("name")String name){
-        return hiService.hi(name);
+    public Map<String, String> hi(@PathVariable("name") String name) {
+        Map<String, String> map = new HashMap<>(1);
+        map.put("msg", hiService.hi(name));
+        return map;
     }
 
     @GetMapping("/test")
-    public String test() {
-        return "hello world";
+    public Map<String, String> test() {
+        Map<String, String> map = new HashMap<>(1);
+        map.put("msg", "hello world");
+        return map;
     }
 }
