46d47d38348c548b8c2e67010637d9723279a243
Yibo <xuyibo@outlook.com>
2018-09-06 10:40:26 +0800

    组件升级

--- anotherprovider/src/main/java/com/yibo/anotherprovider/AnotherProviderApplication.java
+++ anotherprovider/src/main/java/com/yibo/anotherprovider/AnotherProviderApplication.java
@@ -1,9 +1,11 @@
 package com.yibo.anotherprovider;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class AnotherProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(AnotherProviderApplication.class, args);
